1096b758b4e8135ea40346364e7ce1a43b523a46
Pedro Francisco <pedrofsilvafernandes@hotmail.com>
2025-05-01 17:03:10 -0300

    Criando as rotas  get e post (user)

--- Node-TS/src/index.ts
+++ Node-TS/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World update");
+import express from 'express';
+import { Request, Response } from 'express';
+import { u } from 'framer-motion/dist/types.d-B50aGbjN';
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'DioBankAPI' });
+  });
+
+server.post('/user', (req: Request, res: Response) => {
+    const body = req.body
+    console.log(body);
+    res.json({ message: 'User created', user: body });
+})
+
+
+server.listen(5000, () => 
+    console.log('Server is running on port 5000'))
